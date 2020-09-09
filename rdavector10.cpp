5d2dfa5f55423ec5396f4de148a92bbd31761303
Vladimir Nagaev <nagaevvd1289@gmail.com>
2020-09-10 01:25:54 +0400

    something is moving

--- rdavector10.cpp
+++ rdavector10.cpp
@@ -31,8 +31,22 @@ RdaVector10::RdaVector10()
         return m_isValid;
     }
 
+    int RdaVector10::getAsAnotherVector(){
+        return m_isAsVector;
+    }
+    void RdaVector10::setAsAnotherVector(int exVector){
+        m_isAsVector = exVector;
+    }
+
     void RdaVector10::setValidnes(bool validnes){
         m_isValid = validnes;
+    }
+
+    bool RdaVector10::isNull(){
+        return m_isNull;
+    }
+    void RdaVector10::nullThisVector(bool status){
+        m_isNull = !status
     }
 
     QByteArray RdaVector10::getBytes(){
@@ -42,25 +56,49 @@ RdaVector10::RdaVector10()
     QString RdaVector10::toText(){
         QString text = "";
 
-        if(!m_TimeOfThisVector.isNull()){
-            text.append(m_TimeOfThisVector.toString("dd:hh:mm:ss:MMMM"));
-            text.append(" ");
-        }else {
-            text.append(" |");
-        }
-        text.append(" vec# ");
-        text.append(m_vectorNumber);
-        text.append(" P(MPa) ");
-        text.append(m_pressure);
-        text.append(" Tp ");
-        text.append(m_adc_TP);
-        text.append(" ADC_P ");
-        text.append(m_adcPressureData);
-        if (m_isValid){
-            text.append(" valid");
-        }else {
-            text.append(" inValid");
+        if (!m_isNull){
+            if(!m_TimeOfThisVector.isNull()){
+                text.append(m_TimeOfThisVector.toString("dd:hh:mm:ss:MMMM"));
+                text.append(" ");
+            }else {
+                text.append(" |");
             }
+            text.append(" vec# ");
+            text.append(m_vectorNumber);
+            text.append(" P(MPa) ");
+            text.append(m_pressure);
+            text.append(" Tp ");
+            text.append(m_adc_TP);
+            text.append(" ADC_P ");
+            text.append(m_adcPressureData);
+            if (m_isValid){
+                text.append(" valid");
+            }else {
+                text.append(" inValid");
+                }
+
+        }else {
+
+            if(!m_TimeOfThisVector.isNull()){
+                text.append(m_TimeOfThisVector.toString("dd:hh:mm:ss:MMMM"));
+                text.append(" ");
+            }else {
+                text.append(" |");
+            }
+            text.append(" vec# ");
+            text.append(0);
+            text.append(" P(MPa) ");
+            text.append(0);
+            text.append(" Tp ");
+            text.append(0);
+            text.append(" ADC_P ");
+            text.append(0);
+            if (m_isValid){
+                text.append(" inValid/null");
+            }else {
+                text.append(" inValid");
+                }
+    }
 
 
         return text;
@@ -91,12 +129,51 @@ RdaVector10::RdaVector10()
            if (stringlist.at(i).contains("alid")){
                m_isValid = (stringlist.at(i) == "valid");
            }
-
-
        }
-
-
+       if (m_vectorNumber != 0 && m_pressure != 0 && m_adc_TP != 0 && m_adcPressureData != 0 ){
+           m_isNull = true;
+       }else {
+           m_isNull = false;
+}
     }
+
+
+    RdaVector10 RdaVector10::deserializeVectorFromText(QString text){
+        RdaVector10 rdaVector;
+        QStringList stringlist = text.split(" ", QString::SkipEmptyParts);
+        if(stringlist.at(0)!="|"){
+            rdaVector.m_TimeOfThisVector = QDateTime::fromString(stringlist.at(0), "dd:hh:mm:ss:MMMM");
+        }else {
+            //todo do something
+        }
+        for (int i = 1;i < stringlist.size() ;++i ) {
+            if (stringlist.at(i) == "vec#" && i< stringlist.size()-1){
+                rdaVector.m_vectorNumber = stringlist.at(i+1).toInt();
+            }
+            if (stringlist.at(i) == "P(MPa)" && i< stringlist.size()-1){
+                rdaVector.m_pressure = stringlist.at(i+1).toInt();
+            }
+            if (stringlist.at(i) == "Tp" && i< stringlist.size()-1){
+                rdaVector.m_adc_TP = stringlist.at(i+1).toInt();
+            }
+            if (stringlist.at(i) == "ADC_P" && i< stringlist.size()-1){
+                rdaVector.m_adcPressureData = stringlist.at(i+1).toInt();
+            }
+            if (stringlist.at(i).contains("alid")){
+                rdaVector.m_isValid = (stringlist.at(i) == "valid");
+            }
+        }
+        if (rdaVector.m_vectorNumber != 0 && rdaVector.m_pressure != 0 &&
+                rdaVector.m_adc_TP != 0 && rdaVector.m_adcPressureData != 0 ){
+            rdaVector.m_isNull = true;
+        }else {
+            rdaVector.m_isNull = false;
+ }
+
+
+        return rdaVector;
+    }
+
 
     QString RdaVector10::toJson(){
         QString json = "";
@@ -129,5 +206,31 @@ RdaVector10::RdaVector10()
         json.append(m_isValid);
 
         return json;
+    }
+
+
+
+
+    RdaVector10 RdaVector10::fromBytes(QByteArray ourVector){
+        char *buffer;
+        int size;
+        RdaVector10 rdaVector;
+        QByteArray byteArr( buffer, size);
+        QDataStream ds (&byteArr,QIODevice::ReadOnly);
+        ds.setByteOrder(QDataStream::BigEndian);
+
+        if (ourVector.size()!= 10){
+            rdaVector.nullThisVector(true);
+            return rdaVector;
+        }
+
+        for (int i = 0 ; i < 10 ; i++ ) {
+            // TODO Parce this
+           qint16 tmp;
+
+
+        }
+
+        return rdaVector;
     }
 
--- rdavector10.h
+++ rdavector10.h
@@ -5,12 +5,14 @@
 #include <QByteArray>
 #include <QVector>
 #include <QDateTime>
+#include <QDataStream>
 
 
 class RdaVector10
 {
 private:
     int m_vectorNumber;
+    int m_isAsVector;
     QDateTime m_TimeOfThisVector; // date time format dd:hh:mm:ss:MMMM
     QByteArray m_ourVector;
     int m_adcPressureData;
@@ -19,10 +21,12 @@ private:
     int m_pressure; // MPa or Atm
 
     bool m_isValid = true;
+    bool m_isNull = false;
 
 
 public:
     static RdaVector10 fromBytes(QByteArray ourVector);
+    static RdaVector10 deserializeVectorFromText(QString text);
     //static QString fromVector(RdaVector10);
 
     QString toJson();
@@ -40,9 +44,21 @@ public:
 
     int getPressure();
 
+    int getAsAnotherVector();
+    void setAsAnotherVector(int exVector);
+
+
+
     QByteArray getBytes();
 
+    bool isNull();
+    void nullThisVector(bool status);
+
     bool getValidness();
+    void setValidnes(bool validnes);
+
+    void deserializeFromText(QString text);
+    //void deserializeFromJson(QString json);
 
 protected:
 
